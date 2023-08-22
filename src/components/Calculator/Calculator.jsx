72eba9e42fcbe341f87c497e304b02e1da7c282c
Thanoskaloudis <8anos.kal@gmail.com>
2023-08-22 18:53:15 +0300

    create a Calculator components

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './Calculator.scss';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleNumberClick = (number) => {
+    setInput(input + number);
+  };
+
+  const handleOperatorClick = (operator) => {
+    if (input !== '') {
+      setInput(input + operator);
+    }
+  };
+
+  const handleCalculate = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const handleResetInput = () => {
+    setInput('');
+  };
+
+  const handleResetResult = () => {
+    setResult('');
+  };
+
+  return (
+    <div className="calculator">
+      <div className="input">{input}</div>
+      <div className="result">{result}</div>
+      <div className="buttons">
+        {[7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '*', 0].map((value) => (
+          <button key={value} onClick={() => value !== '+' && value !== '*' ? handleNumberClick(value.toString()) : handleOperatorClick(value)}>
+            {value}
+          </button>
+        ))}
+        <button onClick={handleResetInput}>C</button>
+        <button onClick={handleCalculate}>=</button>
+        <button onClick={() => handleOperatorClick('/')}>/</button>
+        <button onClick={handleResetResult}>AC</button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
